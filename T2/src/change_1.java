1541bec3b605f6a73d7ef67e00db1caa413cf223
Mateus Monteiro Ramos <devteus.officeti@gmail.com>
2026-04-10 12:58:46 -0300

    First File

---
+++ T2/src/change_1.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class change_1 {
+    public static void main(String[] args) {
+        Scanner enter = new Scanner(in).useLocale(Locale.US);
+        enter.close();
+    }
+}
